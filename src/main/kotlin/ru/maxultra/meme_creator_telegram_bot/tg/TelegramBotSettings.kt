ce75ea50b52be1f29048fd59a2672898af18082c
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2022-12-04 15:35:37 +0300

    #11 Bot settings storage file

---
+++ src/main/kotlin/ru/maxultra/meme_creator_telegram_bot/tg/TelegramBotSettings.kt
@@ -0,0 +1,4 @@
+package ru.maxultra.meme_creator_telegram_bot.tg
+
+internal const val TELEGRAM_BOT_TOKEN: String = TODO("Place your token here")
+internal const val TELEGRAM_BOT_USERNAME: String = TODO("Place your username here")
